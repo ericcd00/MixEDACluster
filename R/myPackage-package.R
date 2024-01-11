e4eebb6bb97a5ecbe321ebed62b14149e65ecfc2
ericcd00 <cantondominguezeric@gmail.com>
2024-01-11 19:13:58 +0100

    Minor changes

---
+++ R/myPackage-package.R
@@ -0,0 +1,16 @@
+#' @importFrom utils browseURL
+#' @import ggplot2
+#' @import rmarkdown
+#' @import stats
+#' @import hopkins
+#' @import mclust
+#' @import stats
+#' @import kamila
+#' @import clustMixType
+#' @import knitr
+#' @import clusterSim
+#' @import fpc
+#' @import dplyr
+
+NULL
+#> NULL
