df579fcfc35514eddff6427b0bbe9e26ac2f931c
ericcd00 <cantondominguezeric@gmail.com>
2024-01-11 17:59:08 +0100

    Improved MVP, isolated Silhouette Score, added model comparisons and model info

--- R/MixEDACluster.R
+++ R/MixEDACluster.R
@@ -2,13 +2,25 @@
 #' @import rmarkdown
 
 MixEDACluster <- function(data,
-                          output_format = rmarkdown::html_document(theme = "yeti"),
+                          output_format = rmarkdown::html_document(toc = TRUE, theme = "yeti"),
                           mixedData = TRUE,
                           ContVar,
                           CatVar,
                           output_dir = getwd(),
                           output_file = "report.html",
-                          Title = "Data Profiling Report") {
+                          set_title = "Clustering Report") {
+  # Sanity checks
+  if (missing(data) || missing(ContVar) || missing(CatVar)) {
+    stop("There are missing parameters. Check if all the parameters are filled.")
+  }
+
+  if (!is.character(ContVar) || length(ContVar) == 0) {
+    stop("The parameter *ContVar* must be a string vector.")
+  }
+
+  if (!is.character(CatVar) || length(CatVar) == 0) {
+    stop("The parameter *CatVar* must be a string vector.")
+  }
 
   report_dir <- system.file("rmd/clustering_rmarkdown.rmd",
                             package = "MixEDACluster")
@@ -20,7 +32,7 @@ MixEDACluster <- function(data,
       output_dir = output_dir,
       output_file = output_file,
       params = list(data = data, mixedData = mixedData, ContVar = ContVar,
-                    CatVar = CatVar, set_title = Title)
+                    CatVar = CatVar, set_title = set_title)
     )
   )
 
