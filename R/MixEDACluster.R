e51f65c747e39851549afec59d492ce13dbee76a
ericcd00 <cantondominguezeric@gmail.com>
2024-01-11 13:41:39 +0100

    Created MVP

--- R/MixEDACluster.R
+++ R/MixEDACluster.R
@@ -2,25 +2,28 @@
 #' @import rmarkdown
 
 MixEDACluster <- function(data,
-                          mixedData,
+                          output_format = rmarkdown::html_document(theme = "yeti"),
+                          mixedData = TRUE,
                           ContVar,
                           CatVar,
-                          OutputDir,
-                          OutputFile,
-                          Title) {
+                          output_dir = getwd(),
+                          output_file = "report.html",
+                          Title = "Data Profiling Report") {
 
-  report_dir <- system.file("rmd/clustering_markdown.rmd",
+  report_dir <- system.file("rmd/clustering_rmarkdown.rmd",
                             package = "MixEDACluster")
 
-  suppressWarnings(render(
-    input = report_dir,
-    OutputFile = OutputFile,
-    OutputDir = OutputDir,
-    params = list(data = data, mixedData = mixedData, ContVar = ContVar,
-                  CatVar = CatVar, Title = Title)
-  ))
+  suppressWarnings(
+    rmarkdown::render(
+      input = report_dir,
+      output_format = output_format,
+      output_dir = output_dir,
+      output_file = output_file,
+      params = list(data = data, mixedData = mixedData, ContVar = ContVar,
+                    CatVar = CatVar, set_title = Title)
+    )
+  )
 
-  report_path <- path.expand(file.path(OutputDir, OutputFile))
+  report_path <- path.expand(file.path(output_dir, output_file))
   browseURL(report_path)
-
 }
